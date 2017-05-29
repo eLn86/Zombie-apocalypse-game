74133b5faed881aebb871901ae40687ee1929997
Elnathan Erh <elnathan.erh@gmail.com>
2017-05-29 11:19:51 +0800

    added collision detection for scoreboard

--- scoreboard.js
+++ scoreboard.js
@@ -1,11 +1,40 @@
 var Scoreboard = function(settings) {
   var scoreElement = null;
 
-  
+  function wall() {
+
+    var scoreRect = scoreElement.getBoundingClientRect();
+    var w = parseInt(window.innerWidth);
+    var h = parseInt(window.innerHeight);
+
+    if(scoreRect.bottom > h){
+      scoreElement.style.top = (h-scoreRect.height) + 'px';
+    }
+
+    if(scoreRect.top < 0){
+      scoreElement.style.top = '0px';
+    }
+
+    if(scoreRect.left < 0){
+        scoreElement.style.left = '0px';
+    }
+
+    if(scoreRect.right > w){
+        scoreElement.style.left = ( w - scoreRect.width) + 'px' ;
+    }
+  }
+
+  function move(interactions){
+
+    if(settings.walls){
+      wall();
+    }
+  }
+
   function createScoreboard() {
     // Create the object asset
     scoreElement = document.getElementById('power');
-    scoreElement.style.top = '100px';
+    scoreElement.style.top = '0px';
     scoreElement.style.left = '600px';
     scoreElement.style.height = '100px';
   }
@@ -15,7 +44,7 @@ var Scoreboard = function(settings) {
   }
 
   this.render = function(interactions){
-
+    move(interactions);
   }
 
   init();
